a98e1b454596bb7a1b776a7625891458474df814
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2020-04-18 14:23:29 +0200

    no message

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Supsign\ComposerZip;
 
 use Composer\Config;
@@ -19,29 +20,29 @@ class ScriptHandler
         $outputFolder = (array) $package->getExtra()['outputFolder'] ? (array) $package->getExtra()['outputFolder'] : [];
         $vendorPath = $config->get('vendor-dir');
         $rootPath = dirname($vendorPath);
-    //    $filesystem = $filesystem ?: new Filesystem;
+        //    $filesystem = $filesystem ?: new Filesystem;
 
 
-    //  Create Output Folder if not exist
+        //  Create Output Folder if not exist
         $createOutputFolderRealPath = realpath($outputFolder['dir']);
 
-        if ($createOutputFolderRealPath !== false AND is_dir($createOutputFolderRealPath)) {
-            $event->getIO()->write(sprintf('<info>Folder "%s" already exists.<info>',$outputFolder['dir']));
-        }else{
-            $createOutputFolderCmd = "mkdir ".$outputFolder['dir'];
+        if ($createOutputFolderRealPath !== false and is_dir($createOutputFolderRealPath)) {
+            $event->getIO()->write(sprintf('<info>Folder "%s" already exists.<info>', $outputFolder['dir']));
+        } else {
+            $createOutputFolderCmd = "mkdir " . $outputFolder['dir'];
             exec($createOutputFolderCmd);
-            $event->getIO()->write(sprintf('<info>Folder "%s" was created.<info>',$outputFolder['dir']));
+            $event->getIO()->write(sprintf('<info>Folder "%s" was created.<info>', $outputFolder['dir']));
         }
 
 
-     //   var_dump($archives);
+        //   var_dump($archives);
 
         foreach ($archives as $OutputZipName => $RelativeFolderToBeZipped) {
 
             $source = realpath($RelativeFolderToBeZipped);
 
             $zip = new \ZipArchive();
-            $zip->open($outputFolder['dir'].'/'.$OutputZipName.'.zip', \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+            $zip->open($outputFolder['dir'] . '/' . $OutputZipName . '.zip', \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 
             if (is_dir($source)) {
                 $iterator = new \RecursiveDirectoryIterator($source);
@@ -64,45 +65,6 @@ class ScriptHandler
 
 
             $zip->close();
-
-
-            // Remove trailing slash that can cause the target to be deleted by ln.
-     //       $targetRelativePath = rtrim($targetRelativePath, '/');
-
-      //      $sourceAbsolutePath = sprintf('%s/%s', $rootPath, $sourceRelativePath);
-      //      $targetAbsolutePath = sprintf('%s/%s', $rootPath, $targetRelativePath);
-      //      if (!file_exists($sourceAbsolutePath)) {
-      //          continue;
-      //      }
-
-      //      if (file_exists($targetAbsolutePath)) {
-      //          $filesystem->remove($targetAbsolutePath);
-      //      }
-
-      //      $event->getIO()->write(sprintf(
-      //          '<info>Creating symlink for "%s" into "%s"</info>',
-      //          $sourceRelativePath,
-       //         $targetRelativePath
-       //     ));
-
-       //     $targetDirname = dirname($targetAbsolutePath);
-        //    $sourceRelativePath = substr($filesystem->makePathRelative($sourceAbsolutePath, $targetDirname), 0, -1);
-
-        //    $command = 'ln -s';
-        //    if (!$event->isDevMode()) {
-        //        $command = 'cp -r';
-        //    }
-
-            // Escape spaces in path.
-       //     $targetDirname = preg_replace('/(?<!\\))[ ]/', '\\ ', $targetDirname);
-
-            // Build and execute final command.
-       //     $mkdirCmd = 'mkdir -p ' . $targetDirname;
-       //     exec($mkdirCmd);
-       //     $cmd = 'cd ' . $targetDirname . ' && ' . $command . ' ' . $sourceRelativePath . ' ' . basename($targetRelativePath);
-       //     exec($cmd);
-
         }
-        
     }
 }
