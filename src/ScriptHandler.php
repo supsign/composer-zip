e6a383e4334d0017c7fd4e76ace0f189ee5e333f
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2020-04-18 16:09:53 +0200

    no message

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -10,6 +10,31 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class ScriptHandler
 {
+
+    public static function zip_files($source, $destination)
+    {
+        $zip = new \ZipArchive();
+        if ($zip->open($destination, \ZIPARCHIVE::CREATE) === true) {
+            $source = realpath($source);
+            if (is_dir($source)) {
+                $iterator = new \RecursiveDirectoryIterator($source);
+                $iterator->setFlags(\RecursiveDirectoryIterator::SKIP_DOTS);
+                $files = new \RecursiveIteratorIterator($iterator, \RecursiveIteratorIterator::SELF_FIRST);
+                foreach ($files as $file) {
+                    $file = realpath($file);
+                    if (is_dir($file)) {
+                        $zip->addEmptyDir(str_replace($source . DIRECTORY_SEPARATOR, '', $file . DIRECTORY_SEPARATOR));
+                    } elseif (is_file($file)) {
+                        $zip->addFile($file, str_replace($source . DIRECTORY_SEPARATOR, '', $file));
+                    }
+                }
+            } elseif (is_file($source)) {
+                $zip->addFile($source, basename($source));
+            }
+        }
+        return $zip->close();
+    }
+
     public static function createZip(Event $event, Filesystem $filesystem = null)
     {
         /** @var PackageInterface $package */
@@ -39,32 +64,17 @@ class ScriptHandler
 
         foreach ($archives as $OutputZipName => $RelativeFolderToBeZipped) {
 
+
+
+
+
+
+
             $source = realpath($RelativeFolderToBeZipped);
+            $fileName = $OutputZipName . '.zip';
+            $destination = realpath($outputFolder['dir']) . $fileName . '.zip';
 
-            $zip = new \ZipArchive();
-            $zip->open($outputFolder['dir'] . '/' . $OutputZipName . '.zip', \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
-
-            if (is_dir($source)) {
-                $iterator = new \RecursiveDirectoryIterator($source);
-                // skip dot files while iterating 
-                $iterator->setFlags(\RecursiveDirectoryIterator::SKIP_DOTS);
-                $files = new \RecursiveIteratorIterator($iterator, \RecursiveIteratorIterator::SELF_FIRST);
-                var_dump($files);
-                foreach ($files as $file) {
-                    $file = realpath($file);
-                    if (is_dir($file)) {
-                        $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
-                    } else if (is_file($file)) {
-                        $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
-                    }
-                }
-            } else if (is_file($source)) {
-                $zip->addFromString(basename($source), file_get_contents($source));
-            }
-
-
-
-            $zip->close();
+            zip_files($source, $destination);
         }
     }
 }
