507edf55181f08ee6c418393d19ab130decf7df0
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2020-04-19 11:41:04 +0200

    no message

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -5,20 +5,30 @@ namespace Supsign\ComposerZip;
 use Composer\Config;
 use Composer\Package\PackageInterface;
 use Composer\Script\Event;
-use Symfony\Component\Filesystem\Filesystem;
 use Alchemy\Zippy\Zippy;
-
-
-if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
-    require_once dirname(__FILE__) . '/vendor/autoload.php';
-}
-
 
 
 
 
 class ScriptHandler
 {
+
+public static function rrmdir($dir) { 
+    if (is_dir($dir)) { 
+      $objects = scandir($dir);
+      foreach ($objects as $object) { 
+        if ($object != "." && $object != "..") { 
+          if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
+            self::rrmdir($dir. DIRECTORY_SEPARATOR .$object);
+          else
+            unlink($dir. DIRECTORY_SEPARATOR .$object); 
+        } 
+      }
+      rmdir($dir); 
+    } 
+  }
+
+
 
 
     public static function custom_copy($src, $dst) {  
@@ -52,42 +62,6 @@ class ScriptHandler
 
 
 
-public static function copy_files(Event $event){
-
-    $source = 'Develop/HABEBI/Events';
-    $destination = 'Buildtmp/HABEBI/Events';
-    
-    self::custom_copy($source,$destination);
-
-}
-
-
-
-
-    public static function zip_files(Event $event, $source, $destination)
-    {
-        $zip = new \ZipArchive();
-        if ($zip->open($destination, \ZIPARCHIVE::CREATE) === true) {
-            $source = realpath($source);
-            if (is_dir($source)) {
-                $iterator = new \RecursiveDirectoryIterator($source);
-                $iterator->setFlags(\RecursiveDirectoryIterator::SKIP_DOTS);
-                $files = new \RecursiveIteratorIterator($iterator, \RecursiveIteratorIterator::SELF_FIRST);
-                foreach ($files as $file) {
-                    $file = realpath($file);
-                    if (is_dir($file)) {
-                        $zip->addEmptyDir(str_replace($source . DIRECTORY_SEPARATOR, '', $file . DIRECTORY_SEPARATOR));
-                    } elseif (is_file($file)) {
-                        $zip->addFile($file, str_replace($source . DIRECTORY_SEPARATOR, '', $file));
-                    }
-                }
-            } elseif (is_file($source)) {
-                $zip->addFile($source, basename($source));
-            }
-        }
-        $event->getIO()->write(sprintf('<info>Created/Updated "%s" in "%s"<info>', $source, $destination));
-        return $zip->close();
-    }
 
     public static function createZip(Event $event, Filesystem $filesystem = null)
     {
@@ -100,39 +74,102 @@ public static function copy_files(Event $event){
         $vendorPath = $config->get('vendor-dir');
         $rootPath = dirname($vendorPath);
         
-        //    $filesystem = $filesystem ?: new Filesystem;
+
+        // Whole Building Process (just copy it to get rid of symbolic Links Problem)
+        
+        if(!file_exists($outputFolder['build'])) {
+            mkdir($outputFolder['build'], 0777, true);
+            $event->getIO()->write(sprintf('<info>Created Folder "%s"<info>', $outputFolder['build']));
+        }else{
+            $event->getIO()->write(sprintf('<info>Folder "%s" already exists<info>', $outputFolder['build']));
+        }
+
+
+        $file = $outputFolder['build'].'/del.txt';
+
+    if(!is_file($file)){
+    $contents = 'This file is for creating an empty zip';           // Some simple example content.
+    file_put_contents($file, $contents);     // Save our content to the file.
+    }
+
+
+
+        $copy_destination = 'Buildtmp/HABEBI/Events';
+        
+        foreach ($archives as $OutputZipName => $RelativeFolderToBeZipped) {
+
+        $copy_destination = $outputFolder['build'].strstr($RelativeFolderToBeZipped, '/');
+
+        $copy_destination_root_path = str_replace(strrchr($copy_destination,'/'),"",$copy_destination);
+
+        
+        if(!file_exists($copy_destination_root_path)) {
+            mkdir($copy_destination_root_path, 0777, true);
+            $event->getIO()->write(sprintf('<info>Created Folder "%s"<info>', $copy_destination_root_path));
+        }else{
+            $event->getIO()->write(sprintf('<info>Folder "%s" already exists<info>', $copy_destination_root_path));
+        }
+
+        self::custom_copy($RelativeFolderToBeZipped,$copy_destination);        
+
+        }
+
 
 
         //  Create Output Folder if not exist
-        $createOutputFolderRealPath = realpath($outputFolder['dir']);
+        $createOutputFolderRealPath = realpath($outputFolder['archives']);
 
         if ($createOutputFolderRealPath !== false and is_dir($createOutputFolderRealPath)) {
-            $event->getIO()->write(sprintf('<info>Folder "%s" already exists.<info>', $outputFolder['dir']));
+            $event->getIO()->write(sprintf('<info>Folder "%s" already exists.<info>', $outputFolder['archives']));
         } else {
-            $createOutputFolderCmd = "mkdir " . $outputFolder['dir'];
+            $createOutputFolderCmd = "mkdir " . $outputFolder['archives'];
             exec($createOutputFolderCmd);
-            $event->getIO()->write(sprintf('<info>Folder "%s" was created.<info>', $outputFolder['dir']));
+            $event->getIO()->write(sprintf('<info>Folder "%s" was created.<info>', $outputFolder['archives']));
         }
 
 
-
+// Create Zip
         foreach ($archives as $OutputZipName => $RelativeFolderToBeZipped) {
 
-         //   $source = realpath($RelativeFolderToBeZipped);
-         $fileName = $OutputZipName . '.zip';
-         //   $destination = realpath($outputFolder['dir']) . DIRECTORY_SEPARATOR . $fileName;
-         //   self::zip_files($event, $source, $destination);
+        $fileName = $OutputZipName . '.zip';
 
-            $zippy = Zippy::load();
 
-            $archive = $zippy->create($outputFolder['dir'].'/'.$fileName, array(
-                'folder' => 'Develop/HABEBI/Events'
+        $zippy = Zippy::load();
+
+
+
+            $subfolder = str_replace('/','',strrchr($copy_destination,'/'));
+
+            $filesToZip = $files = array_diff(scandir($copy_destination), array('.', '..'));
+
+            foreach ($filesToZip as &$value) {
+                $value = $copy_destination . '/'. $value;
+            }
+            unset($value);
+
+
+
+
+         $archive = $zippy->create($outputFolder['archives'].'/'.$fileName, array(
+              'Buildtmp/del.txt'
             ), true);
 
 
-        
 
 
-        }
+            $archive->addMembers($filesToZip, true);
+            $archive->removeMembers('del.txt');
+            $event->getIO()->write(sprintf('<info>Created "%s"</info>', $outputFolder['archives']));
+
+
+
     }
+    self::rrmdir($outputFolder['build']);
+    $event->getIO()->write(sprintf('<info>Cleaned up and removed Folder "%s"</info>', $outputFolder['archives']));
+
+}
+
+
+
+
 }
